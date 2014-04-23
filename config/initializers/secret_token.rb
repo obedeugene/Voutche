788f7173b0fd42dc68ee90d5ad535cda31799c9a
Obed Eugene <obed.eugene@gmail.com>
2014-04-22 22:05:41 -0400

    added rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voutche::Application.config.secret_key_base = 'd45860b1088a73ca9b3a14954ad077439cc4f760e6839f0b6c6dc9a3745b43d892dc672e7f35b914b4c4c3ae746391b8fadfddb3a4e132ed82ee7ba95f2ea8fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Voutche::Application.config.session_store :cookie_store, key: '_Voutche_session'
