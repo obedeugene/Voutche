5c832bbba83bece1d94e1e60d3c9066c1475c75f
Obed Eugene <obed.eugene@gmail.com>
2014-04-24 12:37:42 -0400

    oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-OmniAuth.config.logger = Rails.logger	
+OmniAuth.config.logger = Rails.logger
 
- Rails.application.config.middleware.use OmniAuth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '359939234135489', '8ec3585ee25704437ecb369e3d71dff2'
 end
