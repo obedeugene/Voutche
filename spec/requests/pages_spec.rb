ccc12f27534fa70d1a30cbe1c6000b027e3d977f
Obed Eugene <obed.eugene@gmail.com>
2014-04-25 18:37:31 -0400

    edit pages_spec

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -12,17 +12,18 @@ describe "Pages" do
   
     # it { should	have_selector('title', text: 'Home') }
     it { should have_selector('h1',    text: 'Voutche') }
+    it { should have_selector('title, text: 'Voutche')}
 end
 
-  it "should have the right links"	do
-  	visit root_path
-  	click_link "About"
-  	it { should have_selector 'title',   text: 'About Us' }
-  	click_link "Help" 
-  	it { should have_selector('title',   text: 'Help') }
-  	click_link "Contact"
-  	it { should have_selector('title',   text: 'Contact Us') }
-  end	
+  # it "should have the right links"	do
+  # 	visit root_path
+  # 	click_link "About"
+  # 	it { should have_selector 'title',   text: 'About Us' }
+  # 	click_link "Help" 
+  # 	it { should have_selector('title',   text: 'Help') }
+  # 	click_link "Contact"
+  # 	it { should have_selector('title',   text: 'Contact Us') }
+  # end	
 
 
 end
