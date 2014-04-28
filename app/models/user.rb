99460947dc4aca1ddb61b88ca36a2aabddb2b1b2
Obed Eugene <obed.eugene@gmail.com>
2014-04-28 08:19:17 -0400

    minor edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,19 @@ class User < ActiveRecord::Base
 	    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 	    user.save!
 	  end
+      
+		def facebook
+		  @facebook ||= Koala::Facebook::API.new(oauth_token)
+		  block_given? ? yield(@facebook) : @facebook
+		rescue Koala::Facebook::APIError => e
+		  logger.info e.to_s
+		  nil # or consider a custom null object
+		end
 
-	  def facebook
-	  end
+		def friends_count
+		  facebook { |fb| fb.get_connection("me", "friends").size }
+		end      
+
 	end
 end
 
